59a9be91f08eb8599dd5747ce546d5883a0c295c
volodia <volodia@mail.bg>
2017-04-11 00:09:59 +0300

    Create Articles

--- blog/src/main/java/softuniBlog/entity/User.java
+++ blog/src/main/java/softuniBlog/entity/User.java
@@ -18,15 +18,22 @@ public class User {
 
     private Set<Role> roles;
 
+    private Set<Article> articles;
+
     public User(String email, String fullName, String password) {
         this.email = email;
         this.password = password;
         this.fullName = fullName;
 
         this.roles = new HashSet<>();
+        this.articles = new HashSet<>();
+
     }
 
-    public User() {    }
+    public User() {
+        this.roles = new HashSet<>();
+        this.articles = new HashSet<>();
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -77,5 +84,14 @@ public class User {
 
     public void addRole(Role role) {
         this.roles.add(role);
+    }
+
+    @OneToMany(mappedBy = "author")
+    public Set<Article> getArticles() {
+        return this.articles;
+    }
+
+    public void setArticles(Set<Article> articles) {
+        this.articles = articles;
     }
 }
